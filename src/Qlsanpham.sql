8eed9c06b4859309ce3ba3d89ec864db1d205138
unknown <khanhbi1998pt@gmail.com>
2022-03-21 13:52:21 +0700

    update1

--- src/Qlsanpham.sql
+++ src/Qlsanpham.sql
@@ -26,3 +26,18 @@ CREATE TABLE OrderDetail(
     FOREIGN KEY (order_ID) REFERENCES CustomerOrder(orderID),
     FOREIGN KEY (product_ID) REFERENCES Product(productID)
 );
+
+INSERT INTO Customer(customerID, customerName, customerAge) VALUES
+(1,'Minh Quan',10),(2,'Ngoc Oanh',20),(3,'Hong Ha',30);
+INSERT INTO CustomerOrder(orderID, orderDate, orderTotalPrice, customer_ID) VALUES
+(1,3/21/2006,NULL,1),
+(2,3/23/2006,NULL,2),
+(3,3/16/2006,NULL,1);
+INSERT INTO Product(productID, productName, productPrice) VALUES
+(1,'May Giat',3),
+(2,'Tu Lanh',5),
+(3,'Dieu Hoa',7),
+(4,'Quat',1),
+(5,'Bep Dien',2);
+INSERT INTO OrderDetail(order_ID, product_ID, orderDetailQTY) VALUES
+(1,1,3),(1,3,7),(1,4,2),(2,1,1),(3,1,8),(2,5,4),(2,3,3);
